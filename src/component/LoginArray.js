274b585a24144c3b616c8ddea16b728d525d69a0
Nicolas <casteranicolas.contact@gmail.com>
2022-03-08 21:53:08 +0100

    data recovery in the "loginArray.js"

--- src/component/LoginArray.js
+++ src/component/LoginArray.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import { NavLink } from 'react-router-dom';
 
 const LoginArray = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [checked, setChecked] = useState(false);
+
+    const sub = e=>{
+        e.preventDefault()
+        console.log(userName, password, checked);
+    }
+
     return (
         <section className="loginArray">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign in</h1>
-            <form>
+            <form onSubmit={sub}>
                 <div className="input-wrapper">
                     <label>Username</label>
-                    <input type="text" id="username" />
+                    <input 
+                    type="text" 
+                    id="username" 
+                    value={userName}
+                    onChange={(e)=>setUserName(e.target.value)}
+                    />
                 </div>
                 <div className="input-wrapper">
                     <label >Password</label>
-                    <input type="password" id="password" />
+                    <input 
+                    type="password" 
+                    id="password" 
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)} 
+                    />
                 </div>
                 <div className="input-remember">
-                    <input type="checkbox" id="remember-me" />
+                    <input type="checkbox" id="remember-me" defaultChecked={checked} onChange={()=>setChecked(!checked)} />
                     <label>Remember me</label>
                 </div>
-                <NavLink to="/login" className="sign-in-button">Sign In</NavLink>
+                <button type="submit" className="sign-in-button" >Sign In</button>
             </form>
         </section>
     );
