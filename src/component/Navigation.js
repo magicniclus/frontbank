6c3a3f420be33188e525dd121200bb77ee2c72e2
Nicolas <casteranicolas.contact@gmail.com>
2022-03-08 15:02:34 +0100

    Creation of the css and contents of the page "HomePage.js"

---
+++ src/component/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <nav className="main-nav">
+                    <NavLink to="/">
+                        <img
+                        className="main-nav-logo-image"
+                        src="./img/argentBankLogo.png"
+                        alt="Argent Bank Logo"
+                        />
+                    </NavLink>
+                    <div>
+                        <NavLink to="/">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                        </NavLink>
+                    </div>
+                </nav>
+    );
+};
+
+export default Navigation;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,68 +1,20 @@
 import React from 'react';
+import Navigation from '../component/Navigation';
 
 const HomePage = () => {
     return (
         <>
             <header>
-                <nav className="main-nav">
-                    <a className="main-nav-logo" href="./index.html">
-                        <img
-                        className="main-nav-logo-image"
-                        src="./img/argentBankLogo.png"
-                        alt="Argent Bank Logo"
-                        />
-                        <h1 className="sr-only">Argent Bank</h1>
-                    </a>
-                    <div>
-                        <a className="main-nav-item" href="./sign-in.html">
-                        <i className="fa fa-user-circle"></i>
-                        Sign In
-                        </a>
-                    </div>
-                </nav>
+                <Navigation />
             </header>
             <main>
-                <div className="hero">
-                    <section className="hero-content">
-                    <h2 className="sr-only">Promoted Content</h2>
-                    <p className="subtitle">No fees.</p>
-                    <p className="subtitle">No minimum deposit.</p>
-                    <p className="subtitle">High interest rates.</p>
-                    <p className="text">Open a savings account with Argent Bank today!</p>
-                    </section>
-                </div>
-                <section className="features">
-                    <h2 className="sr-only">Features</h2>
-                    <div className="feature-item">
-                    <img src="./img/icon-chat.png" alt="Chat Icon" className="feature-icon" />
-                    <h3 className="feature-item-title">You are our #1 priority</h3>
-                    <p>
-                        Need to talk to a representative? You can get in touch through our
-                        24/7 chat or through a phone call in less than 5 minutes.
-                    </p>
-                    </div>
-                    <div className="feature-item">
-                    <img
-                        src="./img/icon-money.png"
-                        alt="Chat Icon"
-                        className="feature-icon"
-                    />
-                    <h3 className="feature-item-title">More savings means higher rates</h3>
-                    <p>
-                        The more you save with us, the higher your interest rate will be!
-                    </p>
-                    </div>
-                    <div className="feature-item">
-                    <img
-                        src="./img/icon-security.png"
-                        alt="Chat Icon"
-                        className="feature-icon"
-                    />
-                    <h3 className="feature-item-title">Security you can trust</h3>
-                    <p>
-                        We use top of the line encryption to make sure your data and money
-                        is always safe.
-                    </p>
+                <section className="canvas">
+                    <div className="canvas-content">
+                        <h1>No fees. <br/>
+                        No minimum deposit. <br/>
+                        High interest rates.
+                        </h1>
+                        <p>Open a savings account with<br/> Argent Bank today!</p>
                     </div>
                 </section>
             </main>
